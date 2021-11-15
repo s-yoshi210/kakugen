1c6ba8d75f4a22fe2dc3f3ae449b14a809b308cc
Yoshitaka Shimazu <y.shimazu210@gmail.com>
2021-11-15 16:12:40 +0900

    gitignore修正

---
+++ resources/views/spa/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="icon" href="/app/favicon.ico">
+    <title>frontend</title>
+  <link href="/app/js/home.js" rel="prefetch"><link href="/app/js/login.js" rel="prefetch"><link href="/app/js/mykakugen.js" rel="prefetch"><link href="/app/js/register.js" rel="prefetch"><link href="/app/js/top.js" rel="prefetch"><link href="/app/js/app.js" rel="preload" as="script"><link href="/app/js/chunk-vendors.js" rel="preload" as="script"></head>
+  <body>
+    <noscript>
+      <strong>We're sorry but frontend doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+    </noscript>
+    <div id="app"></div>
+    <!-- built files will be auto injected -->
+  <script type="text/javascript" src="/app/js/chunk-vendors.js"></script><script type="text/javascript" src="/app/js/app.js"></script></body>
+</html>
