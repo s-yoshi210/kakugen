fbfb2cd3ddc5c4b442f5839b5da4981ff62884d3
Yoshitaka Shimazu <y.shimazu210@gmail.com>
2021-11-17 11:18:35 +0900

    navbar修正

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,41 +1,25 @@
 <template>
   <div id="app" class="mx-auto">
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-danger">
-      <div class="container">
-        <router-link class="navbar-brand" to="/home" v-show="user">Kakugen</router-link>
-        <router-link class="navbar-brand" to="/" v-show="!user">Kakugen</router-link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item" v-show="user">
-              <router-link class="nav-link" to="/home">Home</router-link>
-            </li>
-            <li class="nav-item" v-show="user">
-              <router-link class="nav-link" to="/mykakugen">My格言</router-link>
-            </li>
-            <li class="nav-item" v-show="!user">
-              <router-link class="nav-link" to="/register">アカウント作成</router-link>
-            </li>
-            <li class="nav-item" v-show="!user">
-              <router-link class="nav-link" to="/login">ログイン</router-link>
-            </li>
-            <li class="nav-item" v-show="user">
-              <a href="#" class="nav-link" @click="logout">ログアウト</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div>
+      <b-navbar toggleable="md" type="dark" variant="danger" class="fixed-top">
+        <b-navbar-brand to="/home" v-show="user">Kakugen</b-navbar-brand>
+        <b-navbar-brand to="/" v-show="!user">Kakugen</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/home" v-show="user">Home</b-nav-item>
+            <b-nav-item to="/mykakugen" v-show="user">My格言</b-nav-item>
+            <b-nav-item to="/register" v-show="!user">アカウント作成</b-nav-item>
+            <b-nav-item to="/login" v-show="!user">ログイン</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item @click="logout" v-show="user" right>ログアウト</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
 
     <main role="main" class="container-fluid">
       <router-view/>
@@ -75,7 +59,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, 游ゴシック体, YuGothic, メイリオ, Meiryo, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -93,6 +77,10 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.ml-auto {
+  margin-left: auto!important;
 }
 
 main {
